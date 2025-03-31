993f6a1fb3b8b0fbe3ad387649f1e3959492244c
icebergtsn <zyh2433219116@gmail.com>
2025-03-31 16:19:07 +0800

    feat: add multi-language support

--- src/App.vue
+++ src/App.vue
@@ -3,7 +3,24 @@
 </template>
 
 <script setup lang="ts">
-defineOptions({
-  name: 'App'
-});
+import { supportLanguages } from 'src/i18n';
+import { i18n } from 'src/boot/i18n';
+
+let terminusLanguage = '';
+let terminusLanguageInfo: any = document.querySelector(
+  'meta[name="terminus-language"]'
+);
+if (terminusLanguageInfo && terminusLanguageInfo.content) {
+  terminusLanguage = terminusLanguageInfo.content;
+} else {
+  terminusLanguage = navigator.language;
+}
+
+console.log(navigator.language);
+
+if (terminusLanguage) {
+  if (supportLanguages.find((e) => e.value == terminusLanguage)) {
+    i18n.global.locale.value = terminusLanguage as any;
+  }
+}
 </script>
